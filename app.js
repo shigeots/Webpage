fb72f4782bf09e29ad52072b0b26cde4e938e63f
shigeots <Shigeots@gmail.com>
2023-11-02 20:05:12 +0900

    Add app.js

---
+++ app.js
@@ -0,0 +1,35 @@
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    var galleryImage = document.getElementsByClassName("GalleryImage");
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    function showGalleryImageModal(element) {
+        document.getElementById("modalImage").src = element.src;
+        modal.style.display = "block";
+    }
+    /*
+    function myFunction2() {
+        //alert( 'Hello, world!' );
+        modal.style.display = "block";
+    }
+
+    const demoP = document.getElementById('demoP');
+    
+    demoP?.addEventListener('click', () => {
+        myFunction2();
+    });*/
